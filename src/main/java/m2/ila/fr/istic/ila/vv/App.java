9a715bb3e25f81deb5bbd3467c4ccd5fac795cd1
czbirka <christophe.zbirka@wanadoo.fr>
2017-10-11 11:31:25 +0200

    initial commit for VV tp

---
+++ src/main/java/m2/ila/fr/istic/ila/vv/App.java
@@ -0,0 +1,13 @@
+package m2.ila.fr.istic.ila.vv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
