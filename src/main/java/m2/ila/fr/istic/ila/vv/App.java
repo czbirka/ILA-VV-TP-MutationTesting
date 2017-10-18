325a73a52cbc92b06fad7ebacdd114e91599ff75
czbirka <christophe.zbirka@wanadoo.fr>
2017-10-18 10:02:08 +0200

    first test for javassist

--- src/main/java/m2/ila/fr/istic/ila/vv/App.java
+++ src/main/java/m2/ila/fr/istic/ila/vv/App.java
@@ -1,13 +1,37 @@
 package m2.ila.fr.istic.ila.vv;
 
-/**
- * Hello world!
- *
- */
+import java.lang.Throwable;
+import javassist.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+        try {
+        	ClassPool pool = ClassPool.getDefault();
+        	CtClass modifClass = pool.get("m2.ila.fr.istic.ila.vv.BinOp");
+        	
+//        	modifClass.defrost();
+//        	
+//        	CtMethod m = modifClass.getDeclaredMethod("addition");
+//        	m.insertBefore("{ System.out.println(\"Hello\"); }");
+//        	modifClass.writeFile();
+        	
+        	
+        	CtMethod m = CtNewMethod.make(
+        	                 "public void hello() { System.out.println(\"Hello\"); }",
+        	                 modifClass);
+        	modifClass.addMethod(m);
+        	modifClass.writeFile();
+        	
+        }
+
+        catch(Throwable exc) {
+            System.out.println("Oh, no! Something went wrong.");
+            System.out.println(exc.getMessage());
+            exc.printStackTrace();
+        }
+    	
     }
 }
