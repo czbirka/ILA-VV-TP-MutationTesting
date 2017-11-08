ae9bffa60c44c0c330e33339612a539d9db2de6a
adou <loic.soukpa@gmail.com>
2017-11-08 10:22:31 +0100

    new skeleton

--- src/main/java/m2/ila/fr/istic/ila/vv/App.java
+++ src/main/java/m2/ila/fr/istic/ila/vv/App.java
@@ -1,13 +1,27 @@
 package m2.ila.fr.istic.ila.vv;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	private static final String CLASS_FILES_FOLDER = "somePath";
+	private static final String TEST_SUITE_FOLDER = "anotherPath";
+
+	public static void main(String[] args) {
+//		verifyParams(args);
+		
+	}
+	
+	private static void verifyParams(String[] args) {
+		if (args.length < 2) {
+			System.err.println(
+				"Mutation tool need at least 2 params :\n" + 
+				"1) location of class file(s)\n" + 
+				"2) location of test case(s) to perform" 				
+			);
+			System.exit(-1);
+		}
+
+		for (String input : args) {
+			System.err.println("param : " + input);
+		}
+	}
 }
